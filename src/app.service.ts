a68da2664282e54704f682d9e86ece0020c61e10
Laouali <laoualibachir2000@gmail.com>
2026-02-19 17:42:50 +0100

    refactor: enhance comments in AppService for better clarity

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,3 +1,6 @@
+// This is the AppService class in a NestJS application.
+// It is decorated with the @Injectable() decorator, which allows it to be injected as a dependency into other parts of the application, such as controllers or other services. The getHello() method returns a simple string "Hello World!" when called.
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
